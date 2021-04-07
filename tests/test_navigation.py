835e38203bd06c7200794c9280b7e97ad139b48c
Ultrabug <ultrabug@gentoo.org>
2021-04-07 23:39:07 +0200

    remove useless tests for windows

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -3,12 +3,6 @@ from pathlib import Path
 from mkdocs.structure.files import get_files
 from mkdocs.structure.nav import get_navigation
 
-MAIN_STATIC_NAV = [
-    {"Home": "index.md"},
-    {"Topic1": [{"Named File": str(Path("topic1/named_file.en.md"))}]},
-    {"Topic2": "index.en.md"},
-]
-
 EN_STATIC_NAV = [
     {"Home": "index.md"},
     {"Topic1": [{"Named File": str(Path("topic1/named_file.en.md"))}]},
@@ -32,7 +26,5 @@ def test_plugin_static_nav(config_plugin_static_nav):
     nav = get_navigation(i18n_files, config)
     i18n_plugin.on_nav(nav, config, i18n_files)
     #
-    assert MAIN_STATIC_NAV == EN_STATIC_NAV
-    # assert MAIN_STATIC_NAV == config["nav"]
     assert i18n_plugin.i18n_configs["en"]["nav"] == EN_STATIC_NAV
     assert i18n_plugin.i18n_configs["fr"]["nav"] == FR_STATIC_NAV
