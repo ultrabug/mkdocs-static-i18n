133d3a581d794d0d1ef7f7aa5c7314edb0859630
Ultrabug <ultrabug@gentoo.org>
2021-04-07 22:29:34 +0200

    add tests for static navigation support

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,14 @@ def config_plugin():
 
 
 @pytest.fixture
+def config_plugin_static_nav():
+    site_dir = tempfile.mkdtemp(prefix="mkdocs_tests_")
+    return load_config(
+        "tests/mkdocs_i18n_static_nav.yml", docs_dir="../docs/", site_dir=site_dir
+    )
+
+
+@pytest.fixture
 def config_plugin_no_default_language():
     site_dir = tempfile.mkdtemp(prefix="mkdocs_tests_")
     return load_config(
