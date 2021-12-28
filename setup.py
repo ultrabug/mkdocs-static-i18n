9432cef306ebd42a203cd11a0bfdf6cad712b32f
Ultrabug <ultrabug@ultrabug.net>
2021-12-28 22:47:44 +0100

    version 0.31

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="mkdocs-static-i18n",
-    version="0.30",
+    version="0.31",
     author="Ultrabug",
     author_email="ultrabug@ultrabug.net",
     description="MkDocs i18n plugin using static translation markdown files",
