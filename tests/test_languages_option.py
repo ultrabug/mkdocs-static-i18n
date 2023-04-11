e64e39b860ca6dce04ac57b66c03e81b07206de4
Ultrabug <ultrabug@ultrabug.net>
2023-04-11 18:57:33 +0200

    test(config-languages): update for strict config

--- tests/test_languages_option.py
+++ tests/test_languages_option.py
@@ -9,9 +9,11 @@ def test_plugin_languages_backward_compat_1():
         docs_dir="docs_suffix_structure/",
         plugins={
             "i18n": {
-                "default_language": "en",
-                "languages": {"fr": "français", "en": "english"},
-            }
+                "languages": {
+                    "en": {"name": "english", "default": True},
+                    "fr": {"name": "français"},
+                },
+            },
         },
     )
     i18n_plugin = mkdocs_config["plugins"]["i18n"]
@@ -19,17 +21,17 @@ def test_plugin_languages_backward_compat_1():
     assert i18n_plugin.config["languages"] == {
         "en": {
             "name": "english",
-            "link": "./en/",
+            "link": "./",
             "fixed_link": None,
             "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
+            "default": True,
         },
         "fr": {
             "name": "français",
             "link": "./fr/",
             "fixed_link": None,
             "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
+            "default": False,
         },
     }
 
@@ -40,17 +42,23 @@ def test_plugin_languages_backward_compat_2():
         theme={"name": "mkdocs"},
         use_directory_urls=True,
         docs_dir="docs_suffix_structure/",
-        plugins={"i18n": {"default_language": "en", "languages": {"en": "english"}}},
+        plugins={
+            "i18n": {
+                "languages": {
+                    "en": {"name": "english", "default": True},
+                },
+            },
+        },
     )
     i18n_plugin = mkdocs_config["plugins"]["i18n"]
     i18n_plugin.on_config(mkdocs_config)
     assert i18n_plugin.config["languages"] == {
         "en": {
             "name": "english",
-            "link": "./en/",
+            "link": "./",
             "fixed_link": None,
             "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
+            "default": True,
         },
     }
 
@@ -61,390 +69,30 @@ def test_plugin_languages_backward_compat_3():
         theme={"name": "mkdocs"},
         use_directory_urls=True,
         docs_dir="docs_suffix_structure/",
-        plugins={"i18n": {"default_language": "en", "languages": {"fr": "français"}}},
+        plugins={
+            "i18n": {
+                "languages": {
+                    "en": {"name": "english", "default": True, "build": False},
+                    "fr": {"name": "français"},
+                },
+            },
+        },
     )
     i18n_plugin = mkdocs_config["plugins"]["i18n"]
     i18n_plugin.on_config(mkdocs_config)
     assert i18n_plugin.config["languages"] == {
         "en": {
-            "name": "en",
+            "name": "english",
             "link": "./",
             "fixed_link": None,
             "build": False,
-            "site_name": "MkDocs static i18n plugin tests",
+            "default": True,
         },
         "fr": {
             "name": "français",
             "link": "./fr/",
             "fixed_link": None,
             "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_4():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {"default": {"name": "english_default"}, "fr": "français"},
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english_default",
-            "link": "./",
-            "fixed_link": None,
-            "build": False,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_5():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {"name": "english_default"},
-                    "fr": "français",
-                    "en": {
-                        "name": "english",
-                        "build": True,
-                        "site_name": "English site name",
-                    },
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english",
-            "link": "./en/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "English site name",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_6():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {"name": "english_default"},
-                    "fr": {"name": "français", "link": "/fr"},
-                    "en": {
-                        "name": "english",
-                        "build": False,
-                        "site_name": "MkDocs static i18n plugin tests",
-                    },
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english",
-            "link": "./en/",
-            "fixed_link": None,
-            "build": False,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "/fr",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_7():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {"fr": {"name": "français"}, "en": {"name": "english"}},
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english",
-            "link": "./en/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_8():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {"name": "english_default"},
-                    "fr": "français",
-                    "en": {"name": "english", "build": True},
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english",
-            "link": "./en/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_9():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {"name": "english_default"},
-                    "fr": "français",
-                    "en": {"name": "english", "build": False},
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english",
-            "link": "./en/",
-            "fixed_link": None,
-            "build": False,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_10():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {"name": "english_default", "build": True},
-                    "fr": "français",
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english_default",
-            "link": "./",
-            "fixed_link": None,
-            "build": False,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_11():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {"build": True},
-                    "fr": "français",
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "en",
-            "link": "./",
-            "fixed_link": None,
-            "build": False,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_12():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "fr",
-                "languages": {
-                    "default": {"name": "french_default"},
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.config["languages"] == {
-        "fr": {
-            "name": "french_default",
-            "link": "./",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "MkDocs static i18n plugin tests",
-        },
-    }
-
-
-def test_plugin_languages_backward_compat_13():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        use_directory_urls=True,
-        docs_dir="docs_suffix_structure/",
-        plugins={
-            "i18n": {
-                "default_language": "en",
-                "languages": {
-                    "default": {
-                        "name": "english_default",
-                        "site_name": "Default site name",
-                    },
-                    "fr": {"name": "français", "site_name": "Site en Français"},
-                    "en": {
-                        "name": "english",
-                        "build": True,
-                        "site_name": "English site name",
-                    },
-                },
-            }
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    i18n_plugin.on_config(mkdocs_config)
-    assert i18n_plugin.default_language_options["site_name"] == "Default site name"
-    assert i18n_plugin.config["languages"] == {
-        "en": {
-            "name": "english",
-            "link": "./en/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "English site name",
-        },
-        "fr": {
-            "name": "français",
-            "link": "./fr/",
-            "fixed_link": None,
-            "build": True,
-            "site_name": "Site en Français",
+            "default": False,
         },
     }
