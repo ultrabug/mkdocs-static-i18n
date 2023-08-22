fb4bbf182f147c694cde07b0abbfb6f7b67952de
Ultrabug <ultrabug@ultrabug.net>
2023-08-22 11:41:15 +0200

    test(navigation): refactor to use parametrize

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -37,199 +37,91 @@ TRANSLATED_NAV_NO_DIRECTORY_URLS = {
 }
 
 
-def test_plugin_static_nav():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        docs_dir="docs_suffix_structure/",
-        use_directory_urls=True,
-        nav=[
-            {"Home": "index.md"},
-            {"Topic1": [{"Named File": "topic1/named_file.md"}]},
-            {"Topic2": "topic2/README.md"},
-            {"External": "https://ultrabug.fr"},
-        ],
-        plugins={
-            "i18n": {
-                "languages": [
-                    {
-                        "locale": "en",
-                        "name": "english",
-                        "default": True,
-                    },
-                    {"locale": "fr", "name": "français"},
-                ],
-            },
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    for language in i18n_plugin.all_languages:
-        config = i18n_plugin.on_config(mkdocs_config)
-        #
-        i18n_plugin.current_language = language
-        #
-        files = get_files(config)
-        files = i18n_plugin.on_files(files, config)
-        nav = get_navigation(files, config)
-        nav = i18n_plugin.on_nav(nav, config, files)
-        assert len(mkdocs_config.nav) == len(nav.pages) + 1  # +1 for external link
-        for page in nav.pages:
-            assert page.title in STATIC_NAV_DIRECTORY_URLS[language]
-            assert page.abs_url == STATIC_NAV_DIRECTORY_URLS[language][page.title]
-        assert nav.homepage is not None
-
-
-def test_plugin_static_nav_no_directory():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        docs_dir="docs_suffix_structure/",
-        use_directory_urls=False,
-        nav=[
-            {"Home": "index.md"},
-            {"Topic1": [{"Named File": "topic1/named_file.md"}]},
-            {"Topic2": "topic2/README.md"},
-            {"External": "https://ultrabug.fr"},
-        ],
-        plugins={
-            "i18n": {
-                "languages": [
-                    {
-                        "locale": "en",
-                        "name": "english",
-                        "default": True,
-                    },
-                    {"locale": "fr", "name": "français"},
-                ],
-            },
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    for language in i18n_plugin.all_languages:
-        config = i18n_plugin.on_config(mkdocs_config)
-        #
-        i18n_plugin.current_language = language
-        #
-        files = get_files(config)
-        files = i18n_plugin.on_files(files, config)
-        nav = get_navigation(files, config)
-        nav = i18n_plugin.on_nav(nav, config, files)
-        assert len(mkdocs_config.nav) == len(nav.pages) + 1  # +1 for external link
-        for page in nav.pages:
-            assert page.title in STATIC_NAV_NO_DIRECTORY_URLS[language]
-            assert page.abs_url == STATIC_NAV_NO_DIRECTORY_URLS[language][page.title]
-        assert nav.homepage is not None
-
-
-def test_plugin_translated_nav():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        docs_dir="docs_suffix_structure/",
-        use_directory_urls=True,
-        nav=[
-            {"Home": "index.md"},
-            {"Topic1": [{"Named File": "topic1/named_file.md"}]},
-            {"Topic2": "topic2/README.md"},
-            {"External": "https://ultrabug.fr"},
-        ],
-        plugins={
-            "i18n": {
-                "languages": [
-                    {
-                        "locale": "en",
-                        "default": True,
-                        "name": "english",
-                        "nav_translations": {
-                            "Home": "The Home",
-                            "Named File": "Renamed File",
-                        },
-                    },
-                    {
-                        "locale": "fr",
-                        "name": "français",
-                        "nav_translations": {
-                            "Home": "Accueil",
-                            "Named File": "Fichier Nommé",
-                        },
-                    },
-                ],
-            },
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    for language in i18n_plugin.all_languages:
-        config = i18n_plugin.on_config(mkdocs_config)
-        #
-        i18n_plugin.current_language = language
-        #
-        files = get_files(config)
-        files = i18n_plugin.on_files(files, config)
-        nav = get_navigation(files, config)
-        nav = i18n_plugin.on_nav(nav, config, files)
-        assert len(mkdocs_config.nav) == len(nav.pages) + 1  # +1 for external link
-        for page in nav.pages:
-            assert page.title in TRANSLATED_NAV_DIRECTORY_URLS[language]
-            assert page.abs_url == TRANSLATED_NAV_DIRECTORY_URLS[language][page.title]
-        assert nav.homepage is not None
-
-
-def test_plugin_translated_nav_no_directory():
-    mkdocs_config = load_config(
-        "tests/mkdocs.yml",
-        theme={"name": "mkdocs"},
-        docs_dir="docs_suffix_structure/",
-        use_directory_urls=False,
-        nav=[
-            {"Home": "index.md"},
-            {"Topic1": [{"Named File": "topic1/named_file.md"}]},
-            {"Topic2": "topic2/README.md"},
-            {"External": "https://ultrabug.fr"},
-        ],
-        plugins={
-            "i18n": {
-                "languages": [
-                    {
-                        "locale": "en",
-                        "default": True,
-                        "name": "english",
-                        "nav_translations": {
-                            "Home": "The Home",
-                            "Named File": "Renamed File",
-                        },
-                    },
-                    {
-                        "locale": "fr",
-                        "name": "français",
-                        "nav_translations": {
-                            "Home": "Accueil",
-                            "Named File": "Fichier Nommé",
-                        },
-                    },
-                ],
-            },
-        },
-    )
-    i18n_plugin = mkdocs_config["plugins"]["i18n"]
-    for language in i18n_plugin.all_languages:
-        config = i18n_plugin.on_config(mkdocs_config)
-        #
-        i18n_plugin.current_language = language
-        #
-        files = get_files(config)
-        files = i18n_plugin.on_files(files, config)
-        nav = get_navigation(files, config)
-        nav = i18n_plugin.on_nav(nav, config, files)
-        assert len(mkdocs_config.nav) == len(nav.pages) + 1  # +1 for external link
-        for page in nav.pages:
-            assert page.title in TRANSLATED_NAV_NO_DIRECTORY_URLS[language]
-            assert page.abs_url == TRANSLATED_NAV_NO_DIRECTORY_URLS[language][page.title]
-        assert nav.homepage is not None
-
-
 @pytest.mark.parametrize(
     "plugin_config,use_directory_urls,control_data",
     [
+        (
+            {
+                "docs_structure": "suffix",
+                "languages": [
+                    {
+                        "locale": "en",
+                        "name": "english",
+                        "default": True,
+                    },
+                    {"locale": "fr", "name": "français"},
+                ],
+            },
+            True,
+            STATIC_NAV_DIRECTORY_URLS,
+        ),
+        (
+            {
+                "docs_structure": "suffix",
+                "languages": [
+                    {
+                        "locale": "en",
+                        "name": "english",
+                        "default": True,
+                    },
+                    {"locale": "fr", "name": "français"},
+                ],
+            },
+            False,
+            STATIC_NAV_NO_DIRECTORY_URLS,
+        ),
+        (
+            {
+                "docs_structure": "suffix",
+                "languages": [
+                    {
+                        "locale": "en",
+                        "default": True,
+                        "name": "english",
+                        "nav_translations": {
+                            "Home": "The Home",
+                            "Named File": "Renamed File",
+                        },
+                    },
+                    {
+                        "locale": "fr",
+                        "name": "français",
+                        "nav_translations": {
+                            "Home": "Accueil",
+                            "Named File": "Fichier Nommé",
+                        },
+                    },
+                ],
+            },
+            True,
+            TRANSLATED_NAV_DIRECTORY_URLS,
+        ),
+        (
+            {
+                "docs_structure": "suffix",
+                "languages": [
+                    {
+                        "locale": "en",
+                        "default": True,
+                        "name": "english",
+                        "nav_translations": {
+                            "Home": "The Home",
+                            "Named File": "Renamed File",
+                        },
+                    },
+                    {
+                        "locale": "fr",
+                        "name": "français",
+                        "nav_translations": {
+                            "Home": "Accueil",
+                            "Named File": "Fichier Nommé",
+                        },
+                    },
+                ],
+            },
+            False,
+            TRANSLATED_NAV_NO_DIRECTORY_URLS,
+        ),
         (
             {
                 "docs_structure": "folder",
@@ -314,11 +206,11 @@ def test_plugin_translated_nav_no_directory():
         ),
     ],
 )
-def test_plugin_folder_navigation(plugin_config, use_directory_urls, control_data):
+def test_plugin_navigation(plugin_config, use_directory_urls, control_data):
     mkdocs_config = load_config(
         "tests/mkdocs.yml",
         theme={"name": "mkdocs"},
-        docs_dir="docs_folder_structure/",
+        docs_dir=f"docs_{plugin_config['docs_structure']}_structure/",
         use_directory_urls=use_directory_urls,
         nav=[
             {"Home": "index.md"},
